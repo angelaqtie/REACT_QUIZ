bdb0130bf89128fcc71447845b9545b5e3194fc2
Mikegasti <avelpogi10@gmail.com>
2024-11-19 08:02:36 +0800

    quiz

---
+++ src/questions.jsx
@@ -0,0 +1,112 @@
+export const questions = [
+  {
+    question_aid: 1,
+    questions_question: "Q1 Maganda ba ako?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: true,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 2,
+    questions_question: "Q2 Papasa ba ako kay Ma'am Khate?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: true,
+      },
+      {
+        choice: "Asa ka pa!!",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 3,
+    questions_question: "Q3 Papasok ba ako bukas?",
+    choices: [
+      {
+        choice: "Aba ay OO!!",
+        isCorrect: true,
+      },
+      {
+        choice: "Wag na at tamad ka naman",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: false,
+      },
+      {
+        choice: "Wag na at na bagyo",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 4,
+    questions_question: "Q4 Kumain ka na ba?",
+    choices: [
+      {
+        choice: "Hulaan mo",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi pa",
+        isCorrect: true,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 5,
+    questions_question: "Q5 Magpapagupet ba ako?",
+    choices: [
+      {
+        choice: "Magpagupet ka na",
+        isCorrect: true,
+      },
+      {
+        choice: "Wag at hindi bagay",
+        isCorrect: false,
+      },
+      {
+        choice: "go na basta kalbo",
+        isCorrect: false,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+    ],
+  },
+];
